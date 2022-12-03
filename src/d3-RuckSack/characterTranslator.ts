e9d9ec0c3bd44049d7e7ec41bb99381d858ab275
Mitchell Puersten <mitch.puersten@gmail.com>
2022-12-03 09:14:59 -0500

    Fixed test inputs, added uppercase / lowercase handling.

--- src/d3-RuckSack/characterTranslator.ts
+++ src/d3-RuckSack/characterTranslator.ts
@@ -1,3 +1,25 @@
 export function getCharacterPriority(character: string): number {
-    return 0;
+    let minPriority = 1;
+    let maxPriority = 52;
+    
+    let priority: number;
+    
+    console.log(character);
+    if (isUppercase(character)) {
+        return 1;
+    }
+    else /*lowercase*/ {
+        return 0;
+    }
+
+    if (priority < minPriority || priority > maxPriority) {
+        throw Error("Invalid character (${priority}), must be 1-52");
+    }
+
+    return priority;
 }
+
+function isUppercase(character: string): boolean {
+    return (character == character.toUpperCase());
+}
+
--- tests/d3-RucksackTests/characterTranslator.test.ts
+++ tests/d3-RucksackTests/characterTranslator.test.ts
@@ -4,24 +4,24 @@ describe('Character Translator', () => {
     test('Simple test that should always pass', ()=> {
         let value: number = 1;
         let input: string = 'a';
-        expect(getCharacterPriority('input')).toEqual(value);
+        expect(getCharacterPriority(input)).toEqual(value);
     })
 
     test('Simple test that should always pass', ()=> {
         let value: number = 26;
         let input: string = 'z';
-        expect(getCharacterPriority('input')).toEqual(value);
+        expect(getCharacterPriority(input)).toEqual(value);
     })
 
     test('Simple test that should always pass', ()=> {
         let value: number = 27;
         let input: string = 'A';
-        expect(getCharacterPriority('input')).toEqual(value);
+        expect(getCharacterPriority(input)).toEqual(value);
     })
 
     test('Simple test that should always pass', ()=> {
         let value: number = 52;
         let input: string = 'Z';
-        expect(getCharacterPriority('input')).toEqual(value);
+        expect(getCharacterPriority(input)).toEqual(value);
     })
 })
