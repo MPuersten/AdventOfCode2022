b042ed1335720f34d977d18963f563bc8c33e9a3
Mitchell Puersten <mitch.puersten@gmail.com>
2022-12-04 20:56:04 -0500

    Alter folder name.

--- src/D2-RockPaperScissors/tournamentCalculation.ts
+++
@@ -1,8 +0,0 @@
-import * as fs from 'fs'
-import RpsTrounamentPlan from './rpsTournamentPlan';
-
-let strategyText: string = fs.readFileSync('../assets/d2-input.txt', 'utf-8');
-let plan: RpsTrounamentPlan = new RpsTrounamentPlan(strategyText);
-
-console.log("Before the Elf finishes(XYZ is action to pick):" + plan.getStrategyScore());
-console.log("After the Elf finishes(XYZ is loss/draw/win)" + plan.getSelectionScore());
