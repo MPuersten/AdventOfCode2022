8abaec43964d416b5badb7d26057dae8c5ece231
Mitchell Puersten <mitch.puersten@gmail.com>
2022-12-05 06:53:59 -0500

    Finished shell for rearrange script.

--- src/d5-RearrangeStacks/rearrange.ts
+++ src/d5-RearrangeStacks/rearrange.ts
@@ -1,5 +1,12 @@
+import * as fs from 'fs'
 
+let instructionText: string = fs.readFileSync('../assets/d1-input.txt', 'utf-8');
+let individualInstructionTest: string[] = instructionText.split(/\r?\n/);
 
+let instructions: Instruction[] = [];
+for (let i = 0; i < individualInstructionTest.length; i++) {
+    instructions.push(new Instruction(individualInstructionTest[i]));
+}
 
 export function rearrangeStacks(stacks: string[][], instructions: Instruction[]): string[][] {
 
