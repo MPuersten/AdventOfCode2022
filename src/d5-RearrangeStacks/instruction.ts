28f0d191aba4de37692388fdfa07d7759e5e5bd9
Mitchell Puersten <mitch.puersten@gmail.com>
2022-12-05 07:07:06 -0500

    Finished implementation for command ingestion.

--- src/d5-RearrangeStacks/instruction.ts
+++ src/d5-RearrangeStacks/instruction.ts
@@ -1,12 +1,26 @@
 export default class Instruction {
     private _containersToMove: number;
+    private _from: number;
+    private _to: number;
     
     constructor(instructionText: string) {
+        let words: string[] = instructionText.split(' ');
 
+        this._containersToMove = parseInt(words[1]);
+        this._from = parseInt(words[3]);
+        this._to = parseInt(words[5]);
     }
 
     public get containersToMove(): number {
         return this._containersToMove;
+    }
+
+    public get from(): number {
+        return this._from;
+    }
+
+    public get to(): number {
+        return this._to;
     }
 
     public enact(stacks: string[][]): string[][] {
--- tests/d5-RearrangeStacksTests/instruction.test.ts
+++ tests/d5-RearrangeStacksTests/instruction.test.ts
@@ -2,11 +2,29 @@ import Instruction from "../../src/d5-RearrangeStacks/instruction";
 
 describe('Instruction', () => {
     test('Can construct with text and read containers to move', ()=> {
-        let expectedStartValue: number = 8;
+        let containersToMove: number = 8;
         
         let text: string = "move 8 from 3 to 2"
         let instruction: Instruction = new Instruction(text);
 
-        expect(instruction.containersToMove).toEqual(expectedStartValue);
+        expect(instruction.containersToMove).toEqual(containersToMove);
+    })
+
+    test('Can construct with text and read containers to move', ()=> {
+        let from: number = 3;
+        
+        let text: string = "move 8 from 3 to 2"
+        let instruction: Instruction = new Instruction(text);
+
+        expect(instruction.from).toEqual(from);
+    })
+
+    test('Can construct with text and read containers to move', ()=> {
+        let to: number = 2;
+        
+        let text: string = "move 8 from 3 to 2"
+        let instruction: Instruction = new Instruction(text);
+
+        expect(instruction.to).toEqual(to);
     })
 })
