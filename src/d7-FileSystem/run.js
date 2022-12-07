4af39931ddb8d729ac12cd06e5d9de9b566d5e18
Mitchell Puersten <mitch.puersten@gmail.com>
2022-12-07 02:23:03 -0500

    Solved both, cleaned code, needs refactoring.

--- src/d7-FileSystem/run.js
+++ src/d7-FileSystem/run.js
@@ -8,55 +8,6 @@ var directories = [];
 var currentDirectory = new directory_1.default('/');
 var topDirectory = currentDirectory;
 directories.push(currentDirectory);
-for (var i = 0; i < instructions.length; i++) {
-    var instruction = instructions[i].split(' ');
-    // console.log(instruction);
-    // Command
-    if (instruction[0] === '$') {
-        // console.log('Cmd');
-        // cd
-        if (instruction[1] === 'cd') {
-            // previous directory
-            if (instruction[2] === '..') {
-                // console.log("Moving to parent.");
-                currentDirectory = currentDirectory.ParentDirectory;
-                // console.log(currentDirectory.Name);
-                // console.log("Children:");
-                // console.log(currentDirectory.ChildDirectories);
-            }
-            // directory name
-            else {
-                // console.log("Moving to " + instruction[2]);
-                // console.log("Looking through " + currentDirectory.ChildDirectories.length + " directories.");
-                for (var i_1 = 0; i_1 < currentDirectory.ChildDirectories.length; i_1++) {
-                    // console.log("Looking for " + instruction[2] + " found " + currentDirectory.ChildDirectories[i].Name);
-                    if (currentDirectory.ChildDirectories[i_1].Name === instruction[2]) {
-                        currentDirectory = currentDirectory.ChildDirectories[i_1];
-                    }
-                }
-            }
-        }
-        // ls
-        else {
-            // do nothing
-        }
-    }
-    // Directory
-    else if (instruction[0] === 'dir') {
-        // console.log('Dir');
-        var newDirectory = new directory_1.default(instruction[1]);
-        newDirectory.ParentDirectory = currentDirectory;
-        currentDirectory.ChildDirectories.push(newDirectory);
-        // console.log('Added new directory: ');
-        // console.log(currentDirectory.ChildDirectories);
-    }
-    // File
-    else {
-        // console.log('File');
-        currentDirectory.Files.push(new puzzleFile_1.default(parseInt(instruction[0]), instruction[1]));
-    }
-    //if (i === 15) break;
-}
 var directoriesUnder = [];
 function GetDirectoriesWithSizeUnder(sizeUnder, directory) {
     // console.log("Called for " + directory.Name);
@@ -70,10 +21,74 @@ function GetDirectoriesWithSizeUnder(sizeUnder, directory) {
         directoriesUnder.push(directory);
     }
 }
+var directoriesOver = [];
+function GetDirectoriesWithSizeOver(sizeUnder, directory) {
+    // console.log("Called for " + directory.Name);
+    for (var i = 0; i < directory.ChildDirectories.length; i++) {
+        GetDirectoriesWithSizeOver(sizeUnder, directory.ChildDirectories[i]);
+    }
+    var directorySize = directory.getSumUnder();
+    // console.log("Sum under " + directory.Name + " is " + directorySize);
+    if (directorySize > sizeUnder) {
+        // console.log("Pushing " + directory.Name);
+        directoriesOver.push(directory);
+    }
+}
+for (var i = 0; i < instructions.length; i++) {
+    var instruction = instructions[i].split(' ');
+    // Command
+    if (instruction[0] === '$') {
+        // console.log('Cmd');
+        // cd
+        if (instruction[1] === 'cd') {
+            // previous directory
+            if (instruction[2] === '..') {
+                currentDirectory = currentDirectory.ParentDirectory;
+            }
+            // directory name
+            else {
+                for (var i_1 = 0; i_1 < currentDirectory.ChildDirectories.length; i_1++) {
+                    if (currentDirectory.ChildDirectories[i_1].Name === instruction[2]) {
+                        currentDirectory = currentDirectory.ChildDirectories[i_1];
+                    }
+                }
+            }
+        }
+        // ls
+        else {
+            // do nothing
+        }
+    }
+    // Directory
+    else if (instruction[0] === 'dir') {
+        var newDirectory = new directory_1.default(instruction[1]);
+        newDirectory.ParentDirectory = currentDirectory;
+        currentDirectory.ChildDirectories.push(newDirectory);
+    }
+    // File
+    else {
+        currentDirectory.Files.push(new puzzleFile_1.default(parseInt(instruction[0]), instruction[1]));
+    }
+}
+// Question 1
 GetDirectoriesWithSizeUnder(100000, topDirectory);
 console.log("Num fitting folders: " + directoriesUnder.length);
 var underSum = 0;
 for (var i = 0; i < directoriesUnder.length; i++) {
     underSum += directoriesUnder[i].getSumUnder();
 }
-console.log("Under Sum: " + underSum);
+console.log("Under sum: " + underSum);
+// Question 2
+var usedSpace = topDirectory.getSumUnder();
+console.log("Used Space: " + usedSpace);
+var minSpaceToFreeUp = usedSpace - 40000000;
+console.log("Space to free: " + minSpaceToFreeUp);
+GetDirectoriesWithSizeOver(minSpaceToFreeUp, topDirectory);
+var smallestDirectorySize = 70000000;
+for (var i = 0; i < directoriesOver.length; i++) {
+    var directorySize = directoriesOver[i].getSumUnder();
+    if (directorySize < smallestDirectorySize) {
+        smallestDirectorySize = directorySize;
+    }
+}
+console.log("Total size of Directory to delete: " + smallestDirectorySize);
--- src/d7-FileSystem/run.ts
+++ src/d7-FileSystem/run.ts
@@ -9,10 +9,38 @@ let currentDirectory: Directory = new Directory('/');
 let topDirectory: Directory = currentDirectory;
 directories.push(currentDirectory);
 
+let directoriesUnder: Directory[] = [];
+function GetDirectoriesWithSizeUnder(sizeUnder: number, directory: Directory): void {
+    // console.log("Called for " + directory.Name);
+    for (let i = 0; i < directory.ChildDirectories.length; i++){
+        GetDirectoriesWithSizeUnder(sizeUnder, directory.ChildDirectories[i]);
+    }
+
+    let directorySize: number = directory.getSumUnder();
+    // console.log("Sum under " + directory.Name + " is " + directorySize);
+    if (directorySize < sizeUnder) {
+        // console.log("Pushing " + directory.Name);
+        directoriesUnder.push(directory);
+    }
+}
+
+let directoriesOver: Directory[] = [];
+function GetDirectoriesWithSizeOver(sizeUnder: number, directory: Directory): void {
+    // console.log("Called for " + directory.Name);
+    for (let i = 0; i < directory.ChildDirectories.length; i++){
+        GetDirectoriesWithSizeOver(sizeUnder, directory.ChildDirectories[i]);
+    }
+
+    let directorySize: number = directory.getSumUnder();
+    // console.log("Sum under " + directory.Name + " is " + directorySize);
+    if (directorySize > sizeUnder) {
+        // console.log("Pushing " + directory.Name);
+        directoriesOver.push(directory);
+    }
+}
+
 for (let i = 0; i < instructions.length; i++) {
     let instruction: string[] = instructions[i].split(' ');
-
-    // console.log(instruction);
 
     // Command
     if (instruction[0] === '$') {
@@ -22,18 +50,11 @@ for (let i = 0; i < instructions.length; i++) {
         if (instruction[1] === 'cd') {
             // previous directory
             if (instruction[2] === '..') {
-                // console.log("Moving to parent.");
                 currentDirectory = currentDirectory.ParentDirectory;
-                // console.log(currentDirectory.Name);
-                // console.log("Children:");
-                // console.log(currentDirectory.ChildDirectories);
             }
             // directory name
             else {
-                // console.log("Moving to " + instruction[2]);
-                // console.log("Looking through " + currentDirectory.ChildDirectories.length + " directories.");
                 for (let i = 0; i < currentDirectory.ChildDirectories.length; i++){
-                    // console.log("Looking for " + instruction[2] + " found " + currentDirectory.ChildDirectories[i].Name);
                     if (currentDirectory.ChildDirectories[i].Name === instruction[2]){
                         currentDirectory = currentDirectory.ChildDirectories[i];
                     }
@@ -48,47 +69,43 @@ for (let i = 0; i < instructions.length; i++) {
     }
     // Directory
     else if (instruction[0] === 'dir') {
-        // console.log('Dir');
-        
         let newDirectory: Directory = new Directory(instruction[1]);
         newDirectory.ParentDirectory = currentDirectory;
 
         currentDirectory.ChildDirectories.push(newDirectory);
-
-        // console.log('Added new directory: ');
-        // console.log(currentDirectory.ChildDirectories);
     }
     // File
     else {
-        // console.log('File');
         currentDirectory.Files.push(new PuzzleFile(parseInt(instruction[0]), instruction[1]));
     }
-
-    //if (i === 15) break;
 }
 
-let directoriesUnder: Directory[] = [];
-function GetDirectoriesWithSizeUnder(sizeUnder: number, directory: Directory): void {
-    // console.log("Called for " + directory.Name);
-    for (let i = 0; i < directory.ChildDirectories.length; i++){
-        GetDirectoriesWithSizeUnder(sizeUnder, directory.ChildDirectories[i]);
-    }
-
-    let directorySize: number = directory.getSumUnder();
-    // console.log("Sum under " + directory.Name + " is " + directorySize);
-    if (directorySize < sizeUnder) {
-        // console.log("Pushing " + directory.Name);
-        directoriesUnder.push(directory);
-    }
-
-}
-
+// Question 1
 GetDirectoriesWithSizeUnder(100000, topDirectory);
-console.log("Num fitting folders: " + directoriesUnder.length);
 
 let underSum: number = 0;
 for (let i = 0; i < directoriesUnder.length; i++) {
     underSum += directoriesUnder[i].getSumUnder();
 }
 
-console.log("Under Sum: " + underSum);
+console.log("Under sum: " + underSum);
+
+// Question 2
+let usedSpace: number = topDirectory.getSumUnder();
+console.log("Used Space: " + usedSpace);
+
+let minSpaceToFreeUp: number = usedSpace - 40000000;
+console.log("Space to free: " + minSpaceToFreeUp);
+
+GetDirectoriesWithSizeOver(minSpaceToFreeUp, topDirectory);
+
+let smallestDirectorySize: number = 70000000;
+for (let i = 0; i < directoriesOver.length; i++) {
+    let directorySize: number = directoriesOver[i].getSumUnder()
+
+    if (directorySize < smallestDirectorySize) {
+        smallestDirectorySize = directorySize;
+    }
+}
+
+console.log("Total size of Directory to delete: " + smallestDirectorySize);
