7698c1f6b8f3b1c18c53f12354cf939d446c054f
Mitchell Puersten <mitch.puersten@gmail.com>
2022-12-08 07:30:21 -0500

    Finished Q1, can returned if trees are visible.

--- src/d8-TreeHouse/2dArrayUtilities.ts
+++ src/d8-TreeHouse/2dArrayUtilities.ts
@@ -1,6 +1,42 @@
-export function areSurroundingElementsEvenOrLarger(
-    grid: number[][], 
-    row: number, 
-    col: number): boolean {
+export function areSurroundingElementsEvenOrLarger(grid: number[][], row: number, col: number): boolean {
+    let value: number = grid[row][col];
 
+    // Above
+    let coveredAbove: boolean = false;
+    for (let i = row - 1; i >= 0; i--) {
+        if (grid[i][col] >= value) {
+            coveredAbove = true;
+            break;
+        }
+    }
+
+    // Below
+    let coveredBelow: boolean = false;
+    for (let i = row + 1; i < grid.length; i++) {
+        if (grid[i][col] >= value) {
+            coveredBelow = true;
+            break;
+        }
+    }
+
+    // Left
+    let coveredLeft: boolean = false;
+    for (let i = col - 1; i >= 0; i--) {
+        if (grid[row][i] >= value) {
+            coveredLeft = true;
+            break;
+        }
+    }
+
+    // Right
+    let coveredRight: boolean = false;
+    for (let i = col + 1; i < grid[0].length; i++) {
+        if (grid[row][i] >= value) {
+            coveredRight = true;
+            break;
+        }
+    }
+
+    return (coveredAbove && coveredBelow && coveredLeft && coveredRight);
 }
+
--- src/d8-TreeHouse/run.ts
+++ src/d8-TreeHouse/run.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs'
+import { areSurroundingElementsEvenOrLarger } from './2dArrayUtilities';
 
 let treeRows: string[] = (fs.readFileSync('./assets/d8-input.txt', 'utf-8')).split(/\r?\n/);
 let treeGrid: number[][] = [];
@@ -6,8 +7,6 @@ let treeGrid: number[][] = [];
 for (let i = 0; i < treeRows.length; i++) {
     treeGrid.push(treeRows[i].split("").map(element => parseInt(element)));
 }
-
-console.log(treeGrid);
 
 // Start with O(n^2) time solutions
 let visibleTreeCount: number = 0;
@@ -19,8 +18,9 @@ visibleTreeCount += (treeRows[0].length - 2) * 2;
 // Get inner count
 for (let i = 1; i < treeRows.length - 1; i++) {
     for (let j = 1; j < treeRows[0].length - 1; j++) {
-        // if (areSurroundingElementsLarger(treeGrid, i, j)) visibleTreeCount++;
+        // If trees are NOT hidden, increase visible count
+        if (!areSurroundingElementsEvenOrLarger(treeGrid, i, j)) visibleTreeCount++;
     }
 }
 
-console.log(`Visible tree count: ${treeGrid}`);
+console.log(`Visible tree count: ${visibleTreeCount}`);
