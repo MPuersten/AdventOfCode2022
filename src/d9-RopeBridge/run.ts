e8c8e0b32c48045c218a411bf10646ab0ee287f3
Mitchell Puersten <mitch.puersten@gmail.com>
2022-12-09 11:49:51 -0500

    Fixed names in script.

--- src/d9-RopeBridge/run.ts
+++ src/d9-RopeBridge/run.ts
@@ -4,18 +4,18 @@ import LinkedNodes from './linkedNodes';
 let instructionText: string[] = (fs.readFileSync('./assets/d9-input.txt', 'utf-8')).split(/\r?\n/);
 let instructions: string[][] = instructionText.map(instruction => instruction.split(' '));
 
-let smallGrid: LinkedNodes = new LinkedNodes(2);
+let twoNodeChain: LinkedNodes = new LinkedNodes(2);
 
 for (let i = 0; i < instructions.length; i++) {
-    smallGrid.performMovement(instructions[i][0], parseInt(instructions[i][1]));
+    twoNodeChain.performMovement(instructions[i][0], parseInt(instructions[i][1]));
 }
 
-console.log(`Number of unique tail positions: ${smallGrid.numUniqueTailPositions}`)
+console.log(`Number of unique tail positions: ${twoNodeChain.numUniqueTailPositions}`)
 
-let linkedGrids: LinkedNodes = new LinkedNodes(10);
+let tenNodeChain: LinkedNodes = new LinkedNodes(10);
 
 for (let i = 0; i < instructions.length; i++) {
-    linkedGrids.performMovement(instructions[i][0], parseInt(instructions[i][1]));
+    tenNodeChain.performMovement(instructions[i][0], parseInt(instructions[i][1]));
 }
 
-console.log(`Number of unique long tail positions: ${linkedGrids.numUniqueTailPositions}`)
+console.log(`Number of unique long tail positions: ${tenNodeChain.numUniqueTailPositions}`)
