c3f251522275838e04af1e33577e5c1912da90e0
Mitchell Puersten <mitch.puersten@gmail.com>
2022-12-09 02:13:05 -0500

    Solved question 2 with rewite of Grid object.

--- src/d9-RopeBridge/run.ts
+++ src/d9-RopeBridge/run.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs'
 import Grid from './grid';
+import LinkedGrids from './linkedGrids';
 
 let instructionText: string[] = (fs.readFileSync('./assets/d9-input.txt', 'utf-8')).split(/\r?\n/);
 let instructions: string[][] = instructionText.map(instruction => instruction.split(' '));
@@ -11,3 +12,11 @@ for (let i = 0; i < instructions.length; i++) {
 }
 
 console.log(`Number of unique tail positions: ${staticGrid.numUniqueTailPositions}`)
+
+let linkedGrids: LinkedGrids = new LinkedGrids();
+
+for (let i = 0; i < instructions.length; i++) {
+    linkedGrids.performMovement(instructions[i][0], parseInt(instructions[i][1]));
+}
+
+console.log(`Number of unique long tail positions: ${linkedGrids.numUniqueTailPositions}`)
