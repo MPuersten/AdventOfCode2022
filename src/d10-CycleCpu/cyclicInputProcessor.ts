be448b1a99401b22391a2c75f44993629ed871e1
Mitch P (mobile) <mitch.puersten@gmail.com>
2022-12-10 11:14:04 -0500

    Finished Q2, printing graphics optionally with signalsum.

--- src/d10-CycleCpu/cyclicInputProcessor.ts
+++ src/d10-CycleCpu/cyclicInputProcessor.ts
@@ -3,38 +3,70 @@ export default class CyclicInputProcessor {
     private _cycleCompleted: number = 0;
     private _instructionStep = 0;
     private _midProcess: boolean = false;
+    private _printingEnabled = false;
 
+    private _xMidCycle: number = 1;
     private _x: number = 1;
     
-    constructor(instructions: string[]) {
+    constructor(instructions: string[], printingEnabled?: boolean) {
         this._instructions = instructions;
+        if (printingEnabled)
+            this._printingEnabled = printingEnabled;
     }
 
     get signalStrength() {
-        return this._cycleCompleted * this._x;
+        return this._cycleCompleted * this._xMidCycle;
     }
 
     execute(numCycles: number) {
-        let params: string[] = this._instructions[this._instructionStep].split(' ');
-        if(params[0] === 'addx') {
-            if (this._midProcess) {
-                this._midProcess = false;
+        let row: string = "";
 
-                this._x += parseInt(params[1]);
-                this._instructionStep++;
-            }
-            else /*new add*/ {
-                this._midProcess = true;
-            }
-        }
-        else if (params[0] === 'noop') {
-            this._instructionStep++;
-        }
-        else throw Error(`Bad params: ${params}`);
+        for (let i = 0; i < numCycles; i++) {
+            this._xMidCycle = this._x;
 
-        // Prep for next cycle
-        this._cycleCompleted++;
+            if (this._instructions[this._instructionStep] !== undefined) {
+                let params: string[] = this._instructions[this._instructionStep].split(' ');
+                if(params[0] === 'addx') {
+                    if (this._midProcess) {
+                        this._midProcess = false;
+    
+                        this._x += parseInt(params[1]);
+                        this._instructionStep++;
+                    }
+                    else /*new add*/ {
+                        this._midProcess = true;
+                    }
+    
+                    
+                }
+                else if (params[0] === 'noop') {
+                    this._instructionStep++;
+    
+    
+                }
+                else throw Error(`Bad params: ${params}`);
+            }
+
+            if (this.inSprite(this._cycleCompleted % 40)) {
+                row += "#";
+            }
+            else {
+                row += '.';
+            }
+
+            // Prep for next cycle
+            this._cycleCompleted++;
+
+            if (this._printingEnabled && row.length === 40) {
+                console.log(row);
+                row = "";
+            }
+        }       
     }
 
-
+    inSprite(position: number){
+        return (position === this._xMidCycle ||
+            position === this._xMidCycle - 1 ||
+            position === this._xMidCycle + 1);
+    }
 }
--- src/d10-CycleCpu/run.ts
+++ src/d10-CycleCpu/run.ts
@@ -5,6 +5,36 @@ let instructions: string[] = (fs.readFileSync('./assets/d10-input.txt', 'utf-8')
 let processor: CyclicInputProcessor = new CyclicInputProcessor(instructions);
 
 let cycleSum: number = 0;
-for (let i = 0; i < instructions.length; i++) {
-    
-}
+
+// 20
+processor.execute(20);
+cycleSum += processor.signalStrength;
+
+// 60
+processor.execute(40);
+cycleSum += processor.signalStrength;
+
+// 100
+processor.execute(40);
+cycleSum += processor.signalStrength;
+
+// 140
+processor.execute(40);
+cycleSum += processor.signalStrength;
+
+// 180
+processor.execute(40);
+cycleSum += processor.signalStrength;
+
+// 220
+processor.execute(40);
+cycleSum += processor.signalStrength;
+
+console.log(`SignalSum: ${cycleSum}`);
+
+instructions = [];
+instructions = (fs.readFileSync('./assets/d10-input.txt', 'utf-8')).split(/\r?\n/);
+processor = new CyclicInputProcessor(instructions, true);
+
+// Print graphics
+processor.execute(240);
