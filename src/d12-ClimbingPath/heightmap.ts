ed6b34415f253c92f736ed23ca2e2ee71189baf9
Mitchell Puersten <mitch.puersten@gmail.com>
2022-12-12 18:05:30 -0500

    Completed Q1, refactored to clean and simplify readability.

--- src/d12-ClimbingPath/heightmap.ts
+++ src/d12-ClimbingPath/heightmap.ts
@@ -1,8 +1,10 @@
 import { getCharacterPriority } from "../d3-Rucksack/characterPriorityTranslator";
 import Position from "../d9-RopeBridge/position";
+import ValuePosition from "./valuePosition";
 
 export default class Heightmap {
-    private _heights: number[][] = [];;
+    private _heights: number[][] = [];
+    private _letters: string[][] = [];
     private _startPosition: Position = new Position(0, 0);
     private _endPosition: Position = new Position(0, 0);
     
@@ -15,6 +17,8 @@ export default class Heightmap {
             if (endIndex !== -1) this._endPosition = new Position(i, endIndex);
 
             let row: string[] = heightmapText[i].split('');
+            this._letters.push(row);
+
             let numericHeights: number[] = row.map(height => this.getNumericHeight(height));
             this._heights.push(numericHeights);
         }
@@ -31,46 +35,73 @@ export default class Heightmap {
             let lineOut: string[] = this._heights[i].map(char => char.toString());
             if (i === this._startPosition.X) lineOut[this._startPosition.Y] = 'S';
             if (i === this._endPosition.X) lineOut[this._endPosition.Y] = 'E';
-
-            console.log(lineOut.join(','));
         }
     }
 
     findShortestPathToGoal(): number {
-        let queue: any = [];
-        let visited: any = [];
+        let queue: ValuePosition[] = [];
+        let visited: Position[] = [];
 
-        queue.push([[this._startPosition.X, this._startPosition.Y], 0]);
-        visited.push([this._startPosition.X, this._startPosition.Y]);
+        queue.push(new ValuePosition(this._startPosition.X, this._startPosition.Y, 0));
+        visited.push(new Position(this._startPosition.X, this._startPosition.Y));
+        // console.log(`QUEUE Init: ${queue.length}, ${queue[0].X}, ${queue[0].Y}, ${queue[0].Value}`);
             
         while (queue.length > 0) {
-            let itemWithDistance: any = queue.shift();
-            if (itemWithDistance[0][0] === this._endPosition.X &&
-                itemWithDistance[0][1] === this._endPosition.Y) {
-                    return itemWithDistance[1];
+            // console.log('WHILE');
+            let itemWithDistance: ValuePosition = queue.shift()!;
+            if (this.isSamePosition(itemWithDistance as Position, this._endPosition)) {
+                    this.printMapOfPointsListed(visited);
+                    return itemWithDistance.Value;
             }
 
-            let neighbors: Position[] = this.getValidNeighbors(this._startPosition);
+            let neighbors: Position[] = this.getValidNeighbors(itemWithDistance);
             for (let i = 0; i < neighbors.length; i++) {
-                //if (!visited.has([neighbors[i].X, neighbors[i].Y])) {
-                if (!this.inList(visited, [neighbors[i].X, neighbors[i].Y])) {
-                    queue.push([[neighbors[i].X, neighbors[i].Y], itemWithDistance[1] + 1]);
-                    visited.push([neighbors[i].X, neighbors[i].Y]);
-                    console.log(`${neighbors[i].X}, ${neighbors[i].Y}`);
-                    console.log(visited);
+                if (!this.isItemInList(visited, neighbors[i])) {
+                    queue.push(new ValuePosition(neighbors[i].X, neighbors[i].Y, itemWithDistance.Value + 1));
+                    visited.push(new Position(neighbors[i].X, neighbors[i].Y));
                 }
             }
+
+            // console.log(queue);
         }
         
-        throw Error('No Path');
+        this.printMapOfPointsListed(visited);
+        throw Error('No Path Found');
     }
 
-    private inList(list: any, item: number[]): boolean {
+    private printMapOfPointsListed(points: Position[]): void {
+        let out: string[][] = [];
+
+        // Make blank canvas
+        for (let i = 0; i < this._heights.length; i++) {
+            let newRow: string[] = [];
+            for (let j = 0; j < this._heights[0].length; j++) {
+                newRow.push('.');
+            }
+
+            out.push(newRow);
+        }
+
+        // Replace points listed
+        for (let i = 0; i < points.length; i++) {
+            out[points[i].X][points[i].Y] = this._letters[points[i].X][points[i].Y];
+        }
+
+        for (let i = 0; i < out.length; i++){
+            console.log(`${out[i]}`);
+        }
+    }
+
+    private isItemInList(list: Position[], item: Position): boolean {
         for (let i = 0; i < list.length; i++) {
-            if (list[i][0] === item[0] && list[i][1] === item[1]) return true;
+            if (this.isSamePosition(list[i], item)) return true;
         }
 
         return false;
+    }
+
+    private isSamePosition(pos1: Position, pos2: Position): boolean {
+        return (pos1.X === pos2.X && pos1.Y === pos2.Y);
     }
 
     private getValidNeighbors(pos: Position): Position[] {
@@ -78,17 +109,15 @@ export default class Heightmap {
         
         if (pos.X > 0) {
             if (
-                this._heights[pos.X - 1][pos.Y] <= (this._heights[pos.X][pos.Y] + 1) &&
-                this._heights[pos.X - 1][pos.Y] >= (this._heights[pos.X][pos.Y] - 1)
+                (this._heights[pos.X][pos.Y]) - this._heights[pos.X - 1][pos.Y] >= -1
             ) {
                 positions.push(new Position(pos.X - 1, pos.Y));
             }
         }
 
-        if (pos.X < this._heights.length) {
+        if (pos.X < this._heights.length - 1) {
             if (
-                this._heights[pos.X + 1][pos.Y] <= (this._heights[pos.X][pos.Y] + 1) &&
-                this._heights[pos.X + 1][pos.Y] >= (this._heights[pos.X][pos.Y] - 1)
+                (this._heights[pos.X][pos.Y]) - this._heights[pos.X + 1][pos.Y] >= -1
             ) {
                 positions.push(new Position(pos.X + 1, pos.Y));
             }
@@ -96,17 +125,15 @@ export default class Heightmap {
 
         if (pos.Y > 0) {
             if (
-                this._heights[pos.X][pos.Y - 1] <= (this._heights[pos.X][pos.Y] + 1) &&
-                this._heights[pos.X][pos.Y - 1] >= (this._heights[pos.X][pos.Y] - 1)
+                (this._heights[pos.X][pos.Y]) - this._heights[pos.X][pos.Y - 1] >= -1
             ) {
                 positions.push(new Position(pos.X, pos.Y - 1));
             }
         }
 
-        if (pos.Y < this._heights[0].length) {
+        if (pos.Y < this._heights[0].length - 1) {
             if (
-                this._heights[pos.X][pos.Y + 1] <= (this._heights[pos.X][pos.Y] + 1) &&
-                this._heights[pos.X][pos.Y + 1] >= (this._heights[pos.X][pos.Y] - 1)
+                (this._heights[pos.X][pos.Y]) - this._heights[pos.X][pos.Y + 1] >= -1
             ) {
                 positions.push(new Position(pos.X, pos.Y + 1));
             }
