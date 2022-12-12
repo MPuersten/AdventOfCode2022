618cce4d5f4b9004b3d5b6017dfde9f29753de8d
Mitchell Puersten <mitch.puersten@gmail.com>
2022-12-12 08:33:44 -0500

    Started algorithm.

--- src/d12-ClimbingPath/heightmap.ts
+++ src/d12-ClimbingPath/heightmap.ts
@@ -35,4 +35,83 @@ export default class Heightmap {
             console.log(lineOut.join(','));
         }
     }
+
+    findShortestPathToGoal(): number {
+        let queue: any = [];
+        let visited: any = [];
+
+        queue.push([[this._startPosition.X, this._startPosition.Y], 0]);
+        visited.push([this._startPosition.X, this._startPosition.Y]);
+            
+        while (queue.length > 0) {
+            let itemWithDistance: any = queue.shift();
+            if (itemWithDistance[0][0] === this._endPosition.X &&
+                itemWithDistance[0][1] === this._endPosition.Y) {
+                    return itemWithDistance[1];
+            }
+
+            let neighbors: Position[] = this.getValidNeighbors(this._startPosition);
+            for (let i = 0; i < neighbors.length; i++) {
+                //if (!visited.has([neighbors[i].X, neighbors[i].Y])) {
+                if (!this.inList(visited, [neighbors[i].X, neighbors[i].Y])) {
+                    queue.push([[neighbors[i].X, neighbors[i].Y], itemWithDistance[1] + 1]);
+                    visited.push([neighbors[i].X, neighbors[i].Y]);
+                    console.log(`${neighbors[i].X}, ${neighbors[i].Y}`);
+                    console.log(visited);
+                }
+            }
+        }
+        
+        throw Error('No Path');
+    }
+
+    private inList(list: any, item: number[]): boolean {
+        for (let i = 0; i < list.length; i++) {
+            if (list[i][0] === item[0] && list[i][1] === item[1]) return true;
+        }
+
+        return false;
+    }
+
+    private getValidNeighbors(pos: Position): Position[] {
+        let positions: Position[] = [];
+        
+        if (pos.X > 0) {
+            if (
+                this._heights[pos.X - 1][pos.Y] <= (this._heights[pos.X][pos.Y] + 1) &&
+                this._heights[pos.X - 1][pos.Y] >= (this._heights[pos.X][pos.Y] - 1)
+            ) {
+                positions.push(new Position(pos.X - 1, pos.Y));
+            }
+        }
+
+        if (pos.X < this._heights.length) {
+            if (
+                this._heights[pos.X + 1][pos.Y] <= (this._heights[pos.X][pos.Y] + 1) &&
+                this._heights[pos.X + 1][pos.Y] >= (this._heights[pos.X][pos.Y] - 1)
+            ) {
+                positions.push(new Position(pos.X + 1, pos.Y));
+            }
+        }
+
+        if (pos.Y > 0) {
+            if (
+                this._heights[pos.X][pos.Y - 1] <= (this._heights[pos.X][pos.Y] + 1) &&
+                this._heights[pos.X][pos.Y - 1] >= (this._heights[pos.X][pos.Y] - 1)
+            ) {
+                positions.push(new Position(pos.X, pos.Y - 1));
+            }
+        }
+
+        if (pos.Y < this._heights[0].length) {
+            if (
+                this._heights[pos.X][pos.Y + 1] <= (this._heights[pos.X][pos.Y] + 1) &&
+                this._heights[pos.X][pos.Y + 1] >= (this._heights[pos.X][pos.Y] - 1)
+            ) {
+                positions.push(new Position(pos.X, pos.Y + 1));
+            }
+        }
+
+        return positions;
+    }
 }
--- src/d12-ClimbingPath/runs.ts
+++ src/d12-ClimbingPath/runs.ts
@@ -1,7 +1,9 @@
 import * as fs from 'fs'
 import Heightmap from './heightmap';
 
-let heightmapText: string[] = (fs.readFileSync('./assets/d12-input.txt', 'utf-8')).split(/\r?\n/);
+let heightmapText: string[] = (fs.readFileSync('./assets/d12/d12-sample.txt', 'utf-8')).split(/\r?\n/);
+// let heightmapText: string[] = (fs.readFileSync('./assets/d12-input.txt', 'utf-8')).split(/\r?\n/);
 let heightmap: Heightmap = new Heightmap(heightmapText);
 
 heightmap.printGridWithPositions();
+heightmap.findShortestPathToGoal();
