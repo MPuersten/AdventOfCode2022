b4adb23b457f10e2a68f79448e65034e345bb06a
Mitchell Puersten <mitch.puersten@gmail.com>
2022-12-13 06:03:49 -0500

    Added skeleton for dev and test, added files.

---
+++ src/d13-DistressSignal/loadedListComparison.ts
@@ -0,0 +1,3 @@
+export function compareLoadedLists(left: any, right: any) {
+
+}
---
+++ tests/d13-DistressSignalTests/loadedListComparison.test.ts
@@ -0,0 +1,6 @@
+describe('Loaded List Comparison', () => {
+    test('Simple test that should always pass', ()=> {
+        let value: number = 0;
+        expect(value).toEqual(value);
+    })
+})
