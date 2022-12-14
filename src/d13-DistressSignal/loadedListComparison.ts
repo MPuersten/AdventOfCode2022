d97d6b481cf9af8e7c5f6e8a423d012f1be5a472
Mitch P (mobile) <mitch.puersten@gmail.com>
2022-12-13 19:51:18 -0500

    Rewrote isOrderedCorrectly algorithm with cleaner logic.

--- src/d13-DistressSignal/loadedListComparison.ts
+++ src/d13-DistressSignal/loadedListComparison.ts
@@ -1,77 +1,47 @@
-export function isOrderedCorrectly(left: any, right: any): boolean {
-    console.log('left');
-    console.log(left);
-    console.log('right');
-    console.log(right);
-    let leftType: string = typeof(left);
-    let rightType: string = typeof(right);
+export enum Order {
+    Correct,
+    Incorrect,
+    Continue
+}
 
-    // Simple case for basic input
-    if (leftType === 'number' && rightType === 'number') return (left <= right);
-    else if (leftType === 'object' && rightType === 'object') {
-        console.log('both are objects');
-        // Compare elements that are there
-        let allAreOrderedCorrectly: boolean = true;
-        let valuesOrderedCorrectly: boolean = true;
-        
-        for (let i = 0; i < Math.min(left.length, right.length); i++) {
-            console.log('entered loop');
-            if (typeof(left[i]) !== 'number' || typeof(right[i]) !== 'number') {
-                console.log(`One is object`);
-                // edge case for identical arrays
-                // if (left[i].length === right[i].length) {
-                //     let areAllNumbers = true;
-                //     for (let j = 0; j < left[i].length; i++) {
-                //         areAllNumbers = (typeof(left[i][j]) === 'number' && typeof(right[i][j]) === 'number')
-                //     }
+export function isOrderedCorrectly(left: any[], right: any[]): Order {
+    const size: number = Math.max(left.length, right.length);
 
-                //     if (areAllNumbers) {
-                //         for (let j = 0; j < left[i].length; j++) {
-                //             if (left[i][j] > right[i][j]) return false;
-                //         }
-                //     }
-                //     else isOrderedCorrectly(left[i], right[i]);
-                // }
-                allAreOrderedCorrectly = isOrderedCorrectly(left[i], right[i]) && allAreOrderedCorrectly;
-                if(i === Math.min(left.length, right.length) - 1 || !allAreOrderedCorrectly) return allAreOrderedCorrectly;
-            }
-            else if (typeof(left[i]) === 'number' || typeof(right[i]) === 'number') {
-                console.log(left[i]);
-                console.log(right[i]);
-                if (left[i] < right[i]) return true;
-                valuesOrderedCorrectly = valuesOrderedCorrectly && (left[i] === right[i]);
-                console.log(`Updated valuesOrderedCorrectly: ${valuesOrderedCorrectly}`);
-                // if (left[i] < right[i]) return true;
-                // else if (left[i] > right[i]) return false;
-            }
+    for (let i = 0; i < size; i++) {
+        const l = left[i];
+        const r = right[i];
 
-            // console.log(typeof(left[i]));
-            // console.log(typeof(right[i]));
+        if (isNum(l) && isNum(r)) {
+            if(l < r) return Order.Correct;
+            if(l > r) return Order.Incorrect;
         }
-        if (!valuesOrderedCorrectly){
-            console.log('Values not ordered correctly.');
-            return false;
-        } 
+        else if (isObj(l) && isObj(r)) {
+            const result: Order = isOrderedCorrectly(l, r);
+            if (result !== Order.Continue) return result;
+        }
+        else if (isNum(l)) {
+            const result: Order = isOrderedCorrectly([l], r);
+            if (result !== Order.Continue) return result;
+        }
+        else if (isNum(r)) {
+            const result: Order = isOrderedCorrectly(l, [r]);
+            if (result !== Order.Continue) return result;
+        }
+        else if (!l && r) {
+            return Order.Correct;
+        }
+        else if (l && !r) {
+            return Order.Incorrect;
+        }
+    }
 
-        // When elements are equal decide based on list length
-        console.log('Deciding with list lengths');
-        console.log(`left: ${left.length}, right: ${right.length}`);
-        console.log(left);
-        console.log(right);
-        return (left.length <= right.length);
-    }
-    else if (leftType === 'object' && rightType !== 'object') {
-        return isOrderedCorrectly(left, [right]);
-    }
-    else if (leftType !== 'object' && rightType === 'object') {
-        return isOrderedCorrectly([left], right);
-    }
-    else {
-        // Programming / Input Error
-        console.log(left);
-        console.log(right);
-        console.log(`An error occurred, left type: ${leftType}, right type: ${rightType}`);
-    }
-    
-    throw Error();
+    return Order.Continue;
+}
+
+function isNum(element: any) {
+    return (typeof(element) === 'number');
+}
+
+function isObj(element: any) {
+    return (typeof(element) === 'object');
 }
--- src/d13-DistressSignal/run.ts
+++ src/d13-DistressSignal/run.ts
@@ -4,7 +4,7 @@ import { isOrderedCorrectly } from './loadedListComparison';
 // let signalPairText: string[] = (fs.readFileSync('./assets/d13/d13-sample.txt', 'utf-8')).split(/\r?\n/);
 let signalPairText: string[] = (fs.readFileSync('./assets/d13-input.txt', 'utf-8')).split(/\r?\n/);
 
-
+// Create lists
 let lists: any[] = [];
 for (let i = 0; i < signalPairText.length; i += 3) {
     let left: any[] = JSON.parse(signalPairText[i]);
@@ -16,6 +16,7 @@ for (let i = 0; i < signalPairText.length; i += 3) {
     });
 }
 
+// Get Index sum
 let indexSum: number = 0;
 
 for (let i = 0; i < lists.length; i++) {
